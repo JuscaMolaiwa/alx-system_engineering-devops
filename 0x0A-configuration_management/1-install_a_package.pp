43144fb1be241fcd53ea9e6b603f652f30ba20e3
Jusca <jusca.mol@gmail.com>
2024-04-24 04:12:57 -0700

    kill me now

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-exec { 'install_flask_2.1.0':
-  command => '/usr/bin/pip3 install --upgrade flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+# Install a specific version of Flask (2.1.0)
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
