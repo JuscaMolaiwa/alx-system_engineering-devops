52336bb40e545262fd119fdf21cac875f683bffc
Jusca <jusca.mol@gmail.com>
2024-08-16 01:16:16 -0700

    here

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,33 +5,26 @@ import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API."""
-    try:
-        response = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "python:top_ten_script:v1.0.0 (by /u/JuicesKemishi)"},
-                            allow_redirects=False)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "Linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
 
-        # Check for successful request
-        if response.status_code == 200:
-            # Ensure the response content type is JSON
-            if response.headers.get('Content-Type') == 'application/json':
-                data = response.json()
-                posts = data.get('data', {}).get('children', [])
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
 
-                if posts:
-                    for post in posts:
-                        print(post.get('data', {}).get('title', ''))
-                else:
-                    print("None")
-            else:
-                print("Invalid response format")
-        elif response.status_code == 404:
-            print("None")
+    if response.status_code == 404:
+        print("None")
+        return
 
-        elif response.status_code == 403:
-            print("Error: 403 - Blocked.")
-        else:
-            print(f"Error: {response.status_code} - {response.reason}")
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
+
